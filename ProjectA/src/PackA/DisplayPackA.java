a815a4e2c72e7dbdfcf14318e398d4a3acbca962
kirtan <kirtan1902@gmail.com>
2023-06-08 18:07:12 +0530

    deleted

--- ProjectA/src/PackA/DisplayPackA.java
+++
@@ -1,7 +0,0 @@
-package PackA;
-
-public class DisplayPackA {
-	public void disPack() {
-		System.out.println("This is the disPack method of DisplayPackA class of PackA package");
-	}
-}
--- ProjectA/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author kirtan
- *
- */
-module ProjectA {
-}
