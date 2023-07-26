0b1940caf0b76c1092bbbdbb0aeec014733c4389
kirtan <kirtan1902@gmail.com>
2023-07-26 17:34:44 +0530

    changes

---
+++ ProjectA/PackA/DisplayClass.java
@@ -0,0 +1,7 @@
+package PackA;
+
+public class DisplayClass {
+	public void display() {
+		System.out.println("This is the method of DisplayClass of PackA in ProjectA");
+	}
+}
---
+++ ProjectB/PackB/DisplayIn.java
@@ -0,0 +1,13 @@
+package PackB;
+
+import PackA.*;
+
+public class DisplayIn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DisplayClass c = new DisplayClass();
+		c.display();
+	}
+
+}
